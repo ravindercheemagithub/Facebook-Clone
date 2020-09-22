c241dabc08a9a230cff4e2eac97d5115c9ad3f7d
sashitaghimire <ghimiresashita@gmail.com>
2020-09-22 14:37:52 +0545

    header added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
-     <h1>Facebook Clone</h1>
+     {/* header */}
+     <Header />
+
+     {/* appbody */}
+       {/* sidebar */}
+       {/* feed */}
+       {/* widgets */}
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAh1lOLdKPkalZFplIMY7C2QLJzu8kShVA",
+    authDomain: "fb-clone-e685d.firebaseapp.com",
+    databaseURL: "https://fb-clone-e685d.firebaseio.com",
+    projectId: "fb-clone-e685d",
+    storageBucket: "fb-clone-e685d.appspot.com",
+    messagingSenderId: "1060728331320",
+    appId: "1:1060728331320:web:4b52712fca35152b7a1acd",
+    measurementId: "G-Y9PHMV2N8Y"
+  };
